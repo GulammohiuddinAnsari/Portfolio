1987f9d8ae4cfd96dd56e4df863cacac2d37b95c
GulammohiuddinAnsari <gulammohiuddin0712@gmail.com>
2025-02-05 13:32:52 +0530

    education puted in data file

--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -1,8 +1,38 @@
 export const Personal_Description = `
 Hello! I'm Gulam Mohiuddin Ansari, an enthusiastic and skilled recent graduate
           with a BCA in Web Development, equipped with a diverse range
-          of technical skills including PHP, JavaScript, And Learning using the
-          MERN stack to Becoming full-stack web development. Eager to contribute
+          of technical skills including PHP, JavaScript, And Learning MERN stack
+          to Becoming full-stack web development. Eager to contribute
           to a dynamic team and database technologies (SQL, MongoDB)
           in a challenging software development role.
 `
+
+export const EducationData = [
+    {
+      id: 1,
+      course: "BCA Web Development",
+      score: "6.51 CGPA",
+      grade: "First Class",
+      institute: "Veer Narmad South Gujarat University",
+      description:
+        "I pursued a Bachelor's degree in Web Development from Veer Narmad South Gujarat University (Surat), where I gained a strong foundation in various aspects of IT.",
+    },
+    {
+      id: 2,
+      course: "HSC",
+      score: "53.07%",
+      grade: "C1",
+      institute: "RM&VM School, Valsad-396001",
+      description:
+        "I successfully completed my Higher Secondary Education (HSC) from the Gujarat State Board with a notable achievement of securing a 53.07% overall score.",
+    },
+    {
+      id: 3,
+      course: "SSC",
+      score: "58.67%",
+      grade: "C1",
+      institute: "Bai Avabai Heigh School, Valsad-396001",
+      description:
+        "I successfully completed my Secondary School Certificate (SSC) from the Gujarat State Board, achieving an impressive overall percentage of 58.67%. This period of my education laid a strong foundation for my academic journey.",
+    },
+  ];
